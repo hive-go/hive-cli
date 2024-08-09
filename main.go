bc16406d40a4784a619d0281f7cc5e7544c28a43
Italo <55523868+ItaloScript@users.noreply.github.com>
2024-08-08 22:46:02 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{Use: "hive"}
+
+	var generateCmd = &cobra.Command{
+		Use:   "generate_resource [resource name]",
+		Short: "Generate a new resource",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			moduleName := args[0]
+			capitalizedModuleName := capitalize(moduleName)
+			os.Mkdir("modules", 0755)
+			os.Mkdir("modules/"+moduleName, 0755)
+
+			//prepare service
+			//read service.txt in root and replace all occurrences of #MODULE_NAME with moduleName and all occurrences of #CAPITALIZED_NAME with capitalizedModuleName
+			//after it write the content to modules/moduleName/module_name.service.go
+			//start
+			serviceFile, err := os.ReadFile("service.txt")
+			if err != nil {
+				panic(err)
+			}
+			serviceContent := strings.ReplaceAll(string(serviceFile), "#MODULE_NAME", moduleName)
+			serviceContent = strings.ReplaceAll(serviceContent, "#CAPITALIZED_NAME", capitalizedModuleName)
+			os.WriteFile("modules/"+moduleName+"/"+moduleName+".service.go", []byte(serviceContent), 0644)
+
+			//prepare DTO
+			dtoFile, err := os.ReadFile("dto.txt")
+			if err != nil {
+				panic(err)
+			}
+			dtoContent := strings.ReplaceAll(string(dtoFile), "#MODULE_NAME", moduleName)
+			dtoContent = strings.ReplaceAll(dtoContent, "#CAPITALIZED_NAME", capitalizedModuleName)
+			os.WriteFile("modules/"+moduleName+"/"+moduleName+".dto.go", []byte(dtoContent), 0644)
+
+			//prepare controller
+			controllerFile, err := os.ReadFile("controller.txt")
+			if err != nil {
+				panic(err)
+			}
+			controllerContent := strings.ReplaceAll(string(controllerFile), "#MODULE_NAME", moduleName)
+			controllerContent = strings.ReplaceAll(controllerContent, "#CAPITALIZED_NAME", capitalizedModuleName)
+			os.WriteFile("modules/"+moduleName+"/"+moduleName+".controller.go", []byte(controllerContent), 0644)
+
+		},
+	}
+
+	rootCmd.AddCommand(generateCmd)
+	rootCmd.Execute()
+}
+
+func capitalize(s string) string {
+	return strings.ToUpper(s[:1]) + s[1:]
+}
