e074a73c12b6b501f02b50485393291f88ada5d0
Italo <55523868+ItaloScript@users.noreply.github.com>
2024-08-08 23:12:31 -0300

    fix: remove different files

--- main.go
+++ main.go
@@ -7,6 +7,156 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var ControllerString = `package #MODULE_NAME
+
+import (
+	"github.com/hive-go/hive"
+  "github.com/gofiber/fiber/v2"
+)
+
+var #CAPITALIZED_NAMEController = hive.CreateController()
+
+func init() {
+
+	DomainController.SetConfig(hive.ControllerConfig{
+		Prefix: "/#MODULE_NAME",
+	})
+
+	#CAPITALIZED_NAMEController.Post("", func(c *fiber.Ctx) (any, error) {
+
+		payload := new(Create#CAPITALIZED_NAMEDto)
+
+		if err := c.BodyParser(&payload); err != nil {
+			return nil, fiber.NewError(fiber.StatusNotFound, "BODY_PARSING_ERROR")
+		}
+
+		result, err := #CAPITALIZED_NAMEService.Create#CAPITALIZED_NAME(payload)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return result, nil
+	})
+
+	#CAPITALIZED_NAMEController.Get("", func(c *hive.Ctx) (any, error) {
+		result, err := #CAPITALIZED_NAMEService.Get#CAPITALIZED_NAMEs()
+
+		if err != nil {
+			return nil, err
+		}
+
+		return result, nil
+	})
+
+  #CAPITALIZED_NAMEController.Get("/:id", func(c *fiber.Ctx) (any, error) {
+		id := c.Params("id")
+
+		body, err := #CAPITALIZED_NAMEService.Get#CAPITALIZED_NAMEById(id)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return body, nil
+	})
+
+  #CAPITALIZED_NAMEController.Patch("/:id", func(c *fiber.Ctx) (any, error) {
+    id := c.Params("id")
+
+    payload := new(Update#CAPITALIZED_NAMEDto)
+
+    if err := c.BodyParser(&payload); err != nil {
+      return nil, fiber.NewError(fiber.StatusNotFound, "BODY_PARSING_ERROR")
+    }
+
+    body, err := #CAPITALIZED_NAMEService.Update#CAPITALIZED_NAME(id, payload)
+
+    if err != nil {
+      return nil, err
+    }
+
+    return body, nil
+  })
+
+	#CAPITALIZED_NAMEController.Delete("/:id", func(c *fiber.Ctx) (any, error) {
+		id := c.Params("id")
+
+		body, err := #CAPITALIZED_NAMEService.Delete#CAPITALIZED_NAME(id)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return body, nil
+	})
+}
+`
+
+var ServiceString = `package #MODULE_NAME
+
+import (
+  "github.com/gofiber/fiber/v2"
+)
+
+type #CAPITALIZED_NAMEServiceStruct struct{}
+
+var #CAPITALIZED_NAMEService = #CAPITALIZED_NAMEServiceStruct{}
+
+func (u *#CAPITALIZED_NAMEServiceStruct) Create#CAPITALIZED_NAME(data *Create#CAPITALIZED_NAMEDto) (any, error) {
+	return fiber.Map{
+		"message": "#CAPITALIZED_NAME created successfully",
+	}, nil
+}
+
+
+func (u *#CAPITALIZED_NAMEServiceStruct) Get#CAPITALIZED_NAMEs(data *Get#CAPITALIZED_NAMEDto) (any, error) {
+  return fiber.Map{
+    "message": "#CAPITALIZED_NAME retrieved successfully",
+  }, nil
+}
+
+func (u *#CAPITALIZED_NAMEServiceStruct) Get#CAPITALIZED_NAMEById(data *Get#CAPITALIZED_NAMEByIdDto) (any, error) {
+  return fiber.Map{
+    "message": "#CAPITALIZED_NAME retrieved successfully",
+  }, nil
+}
+
+func (u *#CAPITALIZED_NAMEServiceStruct) Update#CAPITALIZED_NAME(data *Update#CAPITALIZED_NAMEDto) (any, error) {
+  return fiber.Map{
+    "message": "#CAPITALIZED_NAME updated successfully",
+  }, nil
+}
+
+func (u *#CAPITALIZED_NAMEServiceStruct) Delete#CAPITALIZED_NAME(data *Delete#CAPITALIZED_NAMEDto) (any, error) {
+  return fiber.Map{
+    "message": "#CAPITALIZED_NAME deleted successfully",
+  }, nil
+}
+`
+
+var ModuleString = `package #MODULE_NAME
+
+import (
+	"github.com/hive-go/hive"
+)
+
+var #CAPITALIZED_NAMEModule = hive.CreateModule()
+
+func init() {
+	#CAPITALIZED_NAMEModule.AddController(#CAPITALIZED_NAMEController)
+}
+`
+
+var DTOString = `package #MODULE_NAME
+
+type Create#CAPITALIZED_NAMEDto struct {
+}
+
+type Update#CAPITALIZED_NAMEDto struct {
+}
+`
+
 func main() {
 	var rootCmd = &cobra.Command{Use: "hive"}
 
